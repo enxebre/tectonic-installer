6824330895e0e0fbafba0d9c027a76c822de6c34
Enxebre <alberto.garcial@hotmail.com>
2017-09-25 18:26:52 +0200

    platform/gcp: injecting provider variables via env

--- platforms/gcp/main.tf
+++ platforms/gcp/main.tf
@@ -15,9 +15,7 @@ limitations under the License.
 */
 
 provider "google" {
-  project     = "${var.tectonic_gcp_project_id}"
   region      = "${var.tectonic_gcp_region}"
-  credentials = "${var.tectonic_gcp_credentials}"
   version     = "0.1.3"
 }
 
@@ -99,7 +97,6 @@ module "etcd" {
 module "masters" {
   source = "../../modules/gcp/master-igm"
 
-  project_id          = "${var.tectonic_gcp_project_id}"
   region              = "${var.tectonic_gcp_region}"
   instance_count      = "${var.tectonic_master_count}"
   zone_list           = "${var.tectonic_gcp_zones}"
